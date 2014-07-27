72737ff35627b112e4533bf0f439ecd6c271bc67
Katherine G. Pe <bridgeutopia@gmail.com>
2014-07-27 15:10:20 +0800

    Note about Bundler.require

--- dependencies.rb
+++ dependencies.rb
@@ -19,4 +19,5 @@ sprockets-sass
 ).each { |d| require d }
 
 $env = ENV['RACK_ENV']
-Bundler.require :default, $env.to_sym
+# Bundler.require :default, $env.to_sym
+# You probably should not do that. See http://myronmars.to/n/dev-blog/2012/12/5-reasons-to-avoid-bundler-require
